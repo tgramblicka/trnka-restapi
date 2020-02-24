cc1c65d7ccf3435bd9aed8db420e109b518398b1
Tomas Gramblicka <t.gramblicka@gmail.com>
2020-02-24 23:23:01 +0100

    SyncDto constructors

--- src/main/java/com/trnka/restapi/dto/SyncDto.java
+++ src/main/java/com/trnka/restapi/dto/SyncDto.java
@@ -1,9 +1,13 @@
 package com.trnka.restapi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class SyncDto {
     private List<StudentDTO> students;

dd7430b9e279a7ff2735fd058f7534ca998c042a
Tomas Gramblicka <t.gramblicka@gmail.com>
2021-05-13 23:41:17 +0200

    SyncConfigDto - updatedOn & createdOn

--- src/main/java/com/trnka/restapi/dto/SyncConfigDto.java
+++ src/main/java/com/trnka/restapi/dto/SyncConfigDto.java
@@ -1,11 +1,30 @@
 package com.trnka.restapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 public class SyncConfigDto {
     private Boolean enableDownloadFromServerToDevice;
     private Boolean enableUploadFromDeviceToServer;
+
+    @JsonFormat(pattern = Constants.API_DATE_FORMAT)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime createdOn;
+
+    @JsonFormat(pattern = Constants.API_DATE_FORMAT)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime updatedOn;
+
+
 }

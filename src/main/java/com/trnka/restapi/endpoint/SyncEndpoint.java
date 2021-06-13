d620bf620e1bfd0a36e4a3e9ae60af27b85cc088
Tomas Gramblicka <t.gramblicka@gmail.com>
2021-06-13 17:27:04 +0200

    SyncEndpoint adapted also syncDto GET with deviceId

--- src/main/java/com/trnka/restapi/endpoint/SyncEndpoint.java
+++ src/main/java/com/trnka/restapi/endpoint/SyncEndpoint.java
@@ -1,13 +1,11 @@
 package com.trnka.restapi.endpoint;
 
-import javax.ws.rs.QueryParam;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.trnka.restapi.dto.StudentDTO;
 import com.trnka.restapi.dto.SyncConfigDto;
 import com.trnka.restapi.dto.SyncDto;
 import com.trnka.restapi.dto.statistics.DeviceStatisticsSyncDto;
@@ -16,11 +14,8 @@ import com.trnka.restapi.dto.statistics.DeviceStatisticsSyncDto;
 @RequestMapping("/vst/sync")
 public interface SyncEndpoint {
 
-    @RequestMapping(method = RequestMethod.GET)
-    StudentDTO getStudent(@QueryParam("studentId") String studentId);
-
     @RequestMapping(method = RequestMethod.GET, path = "all")
-    SyncDto getSyncDto(@QueryParam("deviceId") String deviceId);
+    SyncDto getSyncDto(@RequestParam("deviceId") String deviceId);
 
     @RequestMapping(method = RequestMethod.POST, path = "examination-statistics")
     Boolean updateExaminationStatisticsToAllStudents(@RequestBody DeviceStatisticsSyncDto dto);
